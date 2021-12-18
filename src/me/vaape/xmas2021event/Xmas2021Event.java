7cda5c58ddabf60f34794c2313be618ccfff08c6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 17:29:44 +0000

    Working version of Xmas event (with end bug)

--- src/me/vaape/xmas2021event/Xmas2021Event.java
+++ src/me/vaape/xmas2021event/Xmas2021Event.java
@@ -17,6 +17,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -24,30 +25,70 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowman;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityTeleportEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.SmithingInventory;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+
+import me.vaape.rewards.Rewards;
 import net.md_5.bungee.api.ChatColor;
 
 public class Xmas2021Event extends JavaPlugin implements Listener{
 	
 	private FileConfiguration config = this.getConfig();
-	
+		
 	public static Xmas2021Event instance;
 	private static DamageListener damageListener;
 	
-	public List<Location> spawnPoints;
+	public List<Location> spawnPoints = new ArrayList<Location>();
+	
+	public HashMap<ItemStack, Double> items = new HashMap<ItemStack, Double>();
+	
+	ItemStack goldenApple = new ItemStack(Material.GOLDEN_APPLE); //				1/10
+	ItemStack goldenCarrot = new ItemStack(Material.GOLDEN_CARROT); //				1/10
+	ItemStack pumpkin = new ItemStack(Material.PUMPKIN); //							1/10
+	ItemStack snowBlock = new ItemStack(Material.SNOW_BLOCK); //					1/10
+	ItemStack redDye = new ItemStack(Material.RED_DYE); //							1/10
+	ItemStack greenDye = new ItemStack(Material.GREEN_DYE); //						1/10
+	ItemStack leatherHorseArmor = new ItemStack(Material.LEATHER_HORSE_ARMOR);//	1/50
+	ItemStack leatherHelmet = new ItemStack(Material.LEATHER_HELMET); //			1/50
+	ItemStack leatherChest = new ItemStack(Material.LEATHER_CHESTPLATE); //			1/50
+	ItemStack leatherLegs = new ItemStack(Material.LEATHER_LEGGINGS); //			1/50
+	ItemStack leatherBoots = new ItemStack(Material.LEATHER_BOOTS); //				1/50
+	ItemStack salmon = new ItemStack(Material.SALMON); //							1/10
+	ItemStack cod = new ItemStack(Material.COD); //									1/10
+	ItemStack sweetBerries = new ItemStack(Material.SWEET_BERRIES); //				1/10
+	ItemStack bell = new ItemStack(Material.BELL); //								1/100
+	ItemStack greenWool = new ItemStack(Material.GREEN_WOOL); //					1/10
+	ItemStack redWool = new ItemStack(Material.RED_WOOL); //						1/10
+	ItemStack enchantingPot = new ItemStack(Material.EXPERIENCE_BOTTLE); //			1/10
+	ItemStack eventClock = new ItemStack(Material.CLOCK); //						1/2000
+	ItemStack frozenHeart = new ItemStack(Material.SNOWBALL); //					1/500
+	
 	
 	public static Xmas2021Event getInstance() {
 		return instance;
@@ -62,44 +103,81 @@ public class Xmas2021Event extends JavaPlugin implements Listener{
 		damageListener = new DamageListener();
 	    this.getServer().getPluginManager().registerEvents(damageListener, instance);
 	    
-	    spawnPoints = (List<Location>) config.getList("spawn points");
+	    if (config.getConfigurationSection("spawn points") != null) {
+	    	Set<String> IDs = config.getConfigurationSection("spawn points").getKeys(true);
+		    
+		    for (String ID : IDs) {
+		    	
+		    	int x = config.getInt("spawn points." + ID + ".x");
+		    	int y = config.getInt("spawn points." + ID + ".y");
+		    	int z = config.getInt("spawn points." + ID + ".z");
+		    	
+		    	spawnPoints.add(new Location(Bukkit.getServer().getWorld("world"), x, y, z));
+		    }
+	    }
+	    	    
+	    eventClock.setLore(Collections.singletonList(ChatColor.DARK_PURPLE + "Christmas 2021"));
+	    eventClock.addUnsafeEnchantment(Enchantment.BINDING_CURSE, 1);
+	    eventClock.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+	    ItemMeta clockMeta = eventClock.getItemMeta();
+	    clockMeta.setDisplayName(ChatColor.GREEN + "Christmas Watch");
+	    eventClock.setItemMeta(clockMeta);
 	    
+	    List<String> heartLore = new ArrayList<String>();
+	    heartLore.add("A frozen heart ripped from");
+	    heartLore.add("a snowman's chest");
+	    frozenHeart.setLore(heartLore);
+	    frozenHeart.addUnsafeEnchantment(Enchantment.BINDING_CURSE, 1);
+	    frozenHeart.addItemFlags(ItemFlag.HIDE_ENCHANTS);	    
+	    ItemMeta heartMeta = frozenHeart.getItemMeta();
+	    heartMeta.setDisplayName(ChatColor.GREEN + "Frozen Heart");
+	    frozenHeart.setItemMeta(heartMeta);
+	    
+	    items.put(goldenApple, 0.15);
+	    items.put(goldenCarrot, 0.1);
+	    items.put(pumpkin, 0.1);
+	    items.put(snowBlock, 0.1);
+	    items.put(redDye, 0.1);
+	    items.put(greenDye, 0.1);
+	    items.put(leatherHorseArmor, 0.02);
+	    items.put(leatherHelmet, 0.02);
+	    items.put(leatherChest, 0.02);
+	    items.put(leatherLegs, 0.02);
+	    items.put(leatherBoots, 0.02);
+	    items.put(salmon, 0.1);
+	    items.put(cod, 0.1);
+	    items.put(sweetBerries, 0.1);
+	    items.put(bell, 0.01);
+	    items.put(greenWool, 0.1);
+	    items.put(redWool, 0.1);
+	    items.put(enchantingPot, 0.3);
+	    items.put(eventClock, 0.0006);
+	    items.put(frozenHeart, 0.011);
+	    	    
 	    BukkitRunnable spawnTimer = new BukkitRunnable() {
 	    	
 			@Override
 			public void run() {
 				
-				//Choose random spawn points
-				List<Location> pickedLocations = pickNRandom(spawnPoints, 10);
-				
-				//Only spawn if there are less than 2 snowmen in that location's chunk
-				for (Location spawnPoint : pickedLocations) {
-					
-					Chunk chunk = spawnPoint.getChunk();
-					int numberOfSnowmen = 0;
-					boolean shouldSpawn = true;
-					
-					//Count all snowmen in chunk, if more than 2 then do not spawn
-					for (Entity entity : chunk.getEntities()) {
-						if (entity.getType() == EntityType.SNOWMAN) {
-							numberOfSnowmen ++;
-						}
-						if (numberOfSnowmen > 1) {
-							shouldSpawn = false;
-							break;
-						}
-					}
-					
-					if (shouldSpawn) {
-						if (chunk.isLoaded()) {
-							Bukkit.getServer().broadcastMessage(ChatColor.DARK_GREEN + "Spawning snowman");
-							Snowman snowman = (Snowman) spawnPoint.getWorld().spawnEntity(spawnPoint, EntityType.SNOWMAN);
-							snowman.setHealth(4);
+				//Remove dropped items in spawn older than 20 seconds
+				for (Entity entity : Bukkit.getServer().getWorld("world").getEntities()) {
+					if (entity instanceof Item) {
+						Item item = (Item) entity;
+						
+						if (inSpawn(item.getLocation())) {
+							
+							if (item.getTicksLived() > (20 * 20)) {
+								item.remove();
+							}
 						}
 					}
 				}
 				
-				//Remove all snowmen if more than 30 in spawn
+				if (!(Bukkit.getWorld("world").getTime() > 12500 && Bukkit.getWorld("world").getTime() < 24000)) {
+					return;
+				}
+								
+				//Remove all snowmen if more than 50 in spawn
 				int numberOfSnowmenInSpawn = 0;
 				
 				for (LivingEntity livingEntity : Bukkit.getServer().getWorld("world").getLivingEntities()) {
@@ -109,24 +187,33 @@ public class Xmas2021Event extends JavaPlugin implements Listener{
 					}
 					
 					//If in spawn
-					if (livingEntity.getLocation().getX() > -100 && livingEntity.getLocation().getX() < 250 &&
-						livingEntity.getLocation().getZ() > -100 && livingEntity.getLocation().getZ() < 350) {
+					if (inSpawn(livingEntity.getLocation())) {
 						numberOfSnowmenInSpawn ++;
 					}
 				}
 				
-				if (numberOfSnowmenInSpawn > 30) {
+				if (numberOfSnowmenInSpawn > 25) {
 					for (LivingEntity livingEntity : Bukkit.getServer().getWorld("world").getLivingEntities()) {
 						if (livingEntity.getType() == EntityType.SNOWMAN) {
 							livingEntity.remove();
-							Bukkit.getServer().broadcastMessage(ChatColor.YELLOW + "Removing snowman");
+						}
+					}
+				}
+				
+				for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+					if (inSpawn(player.getLocation())) {
+						
+						if (player.getNearbyEntities(10, 10, 10).size() < 10) {
+							spawnSnowmanRandomly(player);
+							spawnSnowmanRandomly(player);
+							spawnSnowmanRandomly(player);
 						}
 					}
 				}
 			}
 		};
 		
-		spawnTimer.runTaskTimer(instance, 20, 15 * 20);
+		spawnTimer.runTaskTimer(instance, 10 * 20, 15 * 20);
 	}
 	
 	public void loadConfiguration() {
@@ -148,6 +235,7 @@ public class Xmas2021Event extends JavaPlugin implements Listener{
 					Player player = (Player) sender;
 					Location location = player.getLocation();
 					Snowman snowman = (Snowman) location.getWorld().spawnEntity(location, EntityType.SNOWMAN);
+					snowman.setMetadata("event", new FixedMetadataValue(instance, "xmas"));
 					snowman.setHealth(4);
 					return true;
 				}
@@ -158,57 +246,131 @@ public class Xmas2021Event extends JavaPlugin implements Listener{
 			sender.sendMessage(ChatColor.RED + "You don't have permission to do that.");
 		}
 		
-		else if (cmd.getName().equalsIgnoreCase("AddSnowmanPoint")) {
-			if (sender.hasPermission("xmas2021event.addsnowmanpoint")) {
-				if (sender instanceof Player) {
-					Player player = (Player) sender;
-					Location location = player.getLocation();
-
-					spawnPoints.add(location);
-					config.set("spawn points", spawnPoints);
-					saveConfig();
-					
-					player.sendMessage(ChatColor.GREEN + "Successfully added spawnpoint.");
-
-				}
-				else {
-					sender.sendMessage(ChatColor.RED + "You must be a player to do that.");
-				}
-			}
-			else {
-				sender.sendMessage(ChatColor.RED + "You don't have permission to do that.");
-			}
-		}
-		
-		else if (cmd.getName().equalsIgnoreCase("AddFirstSnowmanPoint")) {
-			if (sender.hasPermission("xmas2021event.addsnowmanpoint")) {
-				if (sender instanceof Player) {
-					Player player = (Player) sender;
-					Location location = player.getLocation();
-					List<Location> locations = new ArrayList<>();
-					locations.add(location);
-					
-					config.set("spawn points", locations);
-					saveConfig();
-					
-					spawnPoints = (List<Location>) config.getList("spawn points");
-					
-					player.sendMessage(ChatColor.GREEN + "Successfully added spawnpoint.");
-				}
-				else {
-					sender.sendMessage(ChatColor.RED + "You must be a player to do that.");
-				}
-			}
-			else {
-				sender.sendMessage(ChatColor.RED + "You don't have permission to do that.");
-			}		
-		}	
 		return false;
 	}
+	
+	@EventHandler
+	public void onSnowmanDeath (EntityDeathEvent event) {
+		if (event.getEntity() instanceof Snowman) {
+			if (event.getEntity().getKiller() != null) {
+				Snowman snowman = (Snowman) event.getEntity();
+				OfflinePlayer player = snowman.getKiller();
+				if (snowman.hasMetadata("event")) {
+					
+					double random = Math.random();
+					int numberOfDrops = 1;
+					
+					if (random < 0.7) {
+						numberOfDrops = 1;
+					}
+					else if (random >= 0.7 && random < 0.9) {
+						numberOfDrops = 2;
+					}
+					else if (random >= 0.9) {
+						numberOfDrops = 3;
+					}
+					
+					event.getDrops().clear();
+					
+					for (int i = 1; i <= numberOfDrops; i++) {
+						ItemStack item = pickRandomItem(items);
+						if (item.getType() == Material.SNOWBALL) {
+							Rewards.getInstance().giveReward("xmas2021_frozen_heart", player, false);
+							Bukkit.getServer().broadcastMessage(ChatColor.of("#3b9dff") + "" + ChatColor.ITALIC + player.getName() + " found a Frozen heart...");
+							break;
+						}
+						event.getDrops().add(item);
+					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onChunkLoad (ChunkLoadEvent event) {
+		if (event.getChunk().getX() > -100 && event.getChunk().getX() < 250 &&
+				event.getChunk().getZ() > -100 && event.getChunk().getZ() < 350) {
+			
+			int snowmanCount = 0;
+			for (Entity entity : event.getChunk().getEntities()) {
+				if (entity instanceof Snowman) {
+					snowmanCount ++;
+					if (snowmanCount > 2) {
+						entity.remove();
+						Bukkit.getServer().broadcastMessage("3 snowman in chunk, removing excess snowmen");
+					}
+				}
+			}
+		}
+			
+			
+	}
+	
+	
 	
 	public List<Location> pickNRandom(List<Location> list, int n) {
 		List<Location> copy = new ArrayList<Location>(list);
 	    Collections.shuffle(copy);
 	    return n > copy.size() ? copy.subList(0, copy.size()) : copy.subList(0, n);
+	}
+	
+	public ItemStack pickRandomItem(HashMap<ItemStack, Double> weightedItems) {
+		double total = 0;
+		for (ItemStack item : weightedItems.keySet()) {
+			double weight = weightedItems.get(item);
+			total = total + weight;
+		}
+		double randomSelection = Math.random() * total;
+
+		//Count up from 0 with increment = each individual weight, when random < counter choose that item
+		ItemStack pickedItem = null;
+		double counter = 0;
+		for (ItemStack item : weightedItems.keySet()) {
+			counter = counter + weightedItems.get(item);
+			if (counter >= randomSelection) {
+				pickedItem = item;
+				break;
+			}
+		}
+		return pickedItem;		
+	}
+	
+	public void spawnSnowmanRandomly(Player player) {
+		
+		Location location = player.getLocation();
+		double randomX = (Math.random() - 0.5) * 20; //+-8
+		double randomZ = (Math.random() - 0.5) * 20;
+		
+		location.add(randomX, 5, randomZ);
+		
+		Snowman snowman = (Snowman) Bukkit.getServer().getWorld("world").spawnEntity(location, EntityType.SNOWMAN);
+		snowman.setHealth(3);
+		snowman.setMetadata("event", new FixedMetadataValue(instance, "xmas"));
+	}
+	
+	public boolean inSpawn (Location location) {
+		if (location.getWorld() == Bukkit.getWorld("world")) {
+			if (location.getX() > -100 && location.getX() < 250 &&
+					location.getZ() > -100 && location.getZ() < 350) {
+				return true;
+			}
+			return false;
+		}
+		else return false;
+	}
+	
+	@EventHandler
+	public void onSpawnmanTeleport(EntityTeleportEvent event) {
+		if (event.getEntity() instanceof Snowman) {
+			if (event.getTo().getWorld() == Bukkit.getWorld("world_the_end")) {
+				event.setCancelled(true);
+				event.getEntity().remove();
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onBedInteract (PlayerBedEnterEvent event) {
+		event.setCancelled(true);
 	}
 }
